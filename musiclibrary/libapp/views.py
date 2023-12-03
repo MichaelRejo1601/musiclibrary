a16318bb70318bd607b258ce1d15d73bf5d38196
Michael Rejo <ninjapenguin1601@gmail.com>
2023-12-02 17:48:16 -0700

    fully connecting

--- musiclibrary/libapp/views.py
+++ musiclibrary/libapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import *
 
 # Create your views here.
 
 def test(request):
+    songs = Song.objects.select_related('s_alid__al_aid').all()
     
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {'songs': songs})
