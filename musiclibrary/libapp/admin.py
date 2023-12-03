8602e867a79ca6fc098302d3a8a982896e1e6356
Michael Rejo <ninjapenguin1601@gmail.com>
2023-12-02 17:10:20 -0700

    template and static connection

--- musiclibrary/libapp/admin.py
+++ musiclibrary/libapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Album)
+admin.site.register(models.Artist)
+admin.site.register(models.AuthGroup)
+admin.site.register(models.AuthGroupPermissions)
+admin.site.register(models.AuthPermission)
+admin.site.register(models.AuthUser)
+admin.site.register(models.AuthUserGroups)
+admin.site.register(models.AuthUserUserPermissions)
+admin.site.register(models.Csv)
+admin.site.register(models.Playlist)
+admin.site.register(models.Playsong)
+admin.site.register(models.Song)
+admin.site.register(models.Users)
--- musiclibrary/musiclibrary/settings.py
+++ musiclibrary/musiclibrary/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'musiclibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,9 +121,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_URL = "/musiclibrary/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
