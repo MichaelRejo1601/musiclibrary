521c8f795891965c0eaac69a1a526e51c0a1c6d2
Michael Rejo <ninjapenguin1601@gmail.com>
2023-12-04 14:45:50 -0700

    pushin pull

--- musiclibrary/libapp/views.py
+++ musiclibrary/libapp/views.py
@@ -27,44 +27,51 @@ def home(request):
     return render(request, 'home.html', {"playlists" : playlists})
 
 @login_required
+@csrf_exempt
 def test(request):
     songs = Song.objects.select_related('s_alid__al_aid').all()
     
     return render(request, 'index.html', {'songs': songs})
 
+@login_required
+@csrf_exempt
 def search_artists(request):
     if 'search' in request.GET:
         search_query = request.GET['search']
-        # Use contains so as long as search contains some correct letters we are good
         artists = Artist.objects.filter(a_name__icontains=search_query)
     else:
         artists = []
 
     return render(request, 'artists.html', {'artists': artists})
 
+@login_required
+@csrf_exempt
 def artist_albums(request, artist_id):
     artist = get_object_or_404(Artist, a_aid=artist_id)
     albums = Album.objects.filter(al_aid=artist)
     return render(request, 'artist_albums.html', {'artist': artist, 'albums': albums})
 
+@login_required
+@csrf_exempt
 def search_albums(request):
     if 'search' in request.GET:
         search_query = request.GET['search']
         condition_A = Q(al_name__icontains=search_query)
         condition_B = Q(al_aid__a_name__icontains=search_query)
         combined_condition = condition_A  | condition_B
-        # Use contains so as long as search contains some correct letters we are good
         albums = Album.objects.select_related('al_aid').all().filter(combined_condition)
     else:
         albums = []
 
     return render(request, 'albums.html', {'albums': albums})
 
+@login_required
+@csrf_exempt
 def album_songs(request, album_id):
     album = Album.objects.select_related('al_aid').all().get(al_alid = album_id)
     songs = Song.objects.filter(s_alid=album)
     return render(request, 'album_songs.html', {'album': album, 'songs': songs})
-    
+   
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -77,18 +84,22 @@ def register(request):
             
             user = form.save()
             login(request, user)  # Log the user in after registration
-            return redirect('/login')  # Replace 'home' with your desired redirect path
+            return redirect('/login') 
     else:
         form = UserCreationForm()
 
     return render(request, 'registration/register.html', {'form': form})
 
+@login_required
+@csrf_exempt
 def remove(request, playlist_id):
     obj = Playlist.objects.get(p_pid = playlist_id)
     if obj:
         obj.delete()
     return redirect("/home")
 
+@login_required
+@csrf_exempt
 def playlist(request, playlist_id):
     playlist = get_object_or_404(Playlist, p_pid=playlist_id)
     playlist_instance = Playlist.objects.get(p_pid=playlist_id)
@@ -109,11 +120,15 @@ def playlist(request, playlist_id):
         songs = []
     return render(request, 'playlist.html', {'playlist': playlist, 'playsongs':playsongs, 'songs':songs, 'search_query':search_query})
 
+@login_required
+@csrf_exempt
 def add_song(request, playlist_id, song_id, search_query):
     obj = Playsong(playlist_id, song_id)
     obj.save()
     return redirect("/playlist/{0}/?search={1}".format(playlist_id, search_query))
 
+@login_required
+@csrf_exempt
 def remove_song(request, playlist_id, song_id, search_query):
     obj = Playsong.objects.filter(ps_pid=playlist_id, ps_sid=song_id).first()
     if obj:
